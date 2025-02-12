42e071ee3b9cef7e09e1463f01963a51db5d8e8c
Austin Hamilton <austinhamilton034@gmail.com>
2025-02-12 13:04:36 -0500

    fixed the pipeline for the LSA model

--- main.py
+++ main.py
@@ -2,33 +2,30 @@ from preprocessing import Preprocessor
 from indexing import LSAIndexer
 from summarizer import Summarize
 
+import nltk
+
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+
 #Preprocessor(source_folder : str, language : str('python | js'), granularity : str('method | class | file'))
-p = Preprocessor('test_repo/', 'python', 'method')
-
-
+p = Preprocessor('.', 'python', 'method')
 
 #fills p.documents_ with the documents at the given granularity
+print('Preprocessing...', end='')
 p.preprocess()
+print('Done')
 
+"""summarizer = Summarize(document_list=p.documents_)
 
+print('Generating Summaries...', end='')
+summarizer.generate(show_progress=True, save_file='summaries.log')
+print('Done')"""
 
-documents_list = p.documents_
+summarizer = Summarize.from_file('summaries.log')
 
-# for doc in documents_list:
-#     print(doc)
-    
-suumr = Summarize(documents_list)
-
-print(suumr.summary_generator())
-
-'''
-Add a LLM here to generate a corpus for p.documents_
-You will need to access document.data for each document in p.documents_
-    This will contain the code for each method/class/file in the source folder
-Next you will need to add a LLM with a prompt like "Give a detailed summary of what this code does" or something similar
-    Make sure the result of each prompt is stored in document.corpus, this will let the LSA model work
-    As long as the results of each prompt is stored in document.corpus, everything should be good
-'''
+for i in range(len(p.documents_)):
+    summary = summarizer[str(p.documents_[i])]
+    p.documents_[i].corpus = summary
 
 #gets the optimal model for a set of documents (basically tunes n_components of the LSA model)
 #p.documents_ -> needs to be this
@@ -36,11 +33,15 @@ Next you will need to add a LLM with a prompt like "Give a detailed summary of w
 #max_topics -> maximum value of n_components to be considered
 #dropout_window -> if the model does not improve in dropout_window tries, the process will end early
 #show_progress -> should the progress be shown in a progress bar
-try:
-    model = LSAIndexer.optimal_model(p.documents_, random_state=42, max_topics=150, dropout_window=7, show_progress=True)
-    query = ''
-    result = model.query(query, top_n_results=5)
+print('Building LSA Indexer...', end='')
+umass, model = LSAIndexer.optimal_model(p.documents_, random_state=42, max_topics=150, dropout_window=7, show_progress=True)
+print('Done')
 
-except:
-    pass
-#to query
+print(f'Generated a model with an average umass score of {umass}')
+
+#should give the __search method of the Preprocessor class
+query = 'look for files with a certain extension'
+result = model.query(query, top_n_results=5)
+
+for document in result:
+    print(document)
--- summarizer.py
+++ summarizer.py
@@ -1,33 +1,55 @@
-
 import ollama
-
-
+from tqdm import tqdm
+import pickle
 
 class Summarize:
     def __init__(self, document_list):
-        self.document_list = document_list
-        self.corpus = []
+        self.documents_ = document_list
+        self.summaries_ = {}
+
+    def add_docs(self, document_list):
+        for doc in document_list:
+            self.documents_.append(doc)
     
-    def summary_generator(self):
-        
-        summarized_data = {}
+    def generate(self, show_progress=False, save_file=None):
         generation_kwargs = {
-        "max_tokens":150, # Max number of new tokens to generate
-        # "stop":["<|endoftext|>", "</s>"], # Text sequences to stop generation on
-        "echo":True, # Echo the prompt in the output
-        "top_k":1 # This is essentially greedy decoding, since the model will always return the highest-probability token. Set this value > 1 for sampling decoding
-            }
+            "max_tokens":250, # Max number of new tokens to generate
+            # "stop":["<|endoftext|>", "</s>"], # Text sequences to stop generation on
+            "echo":True, # Echo the prompt in the output
+            "top_k":1 # This is essentially greedy decoding, since the model will always return the highest-probability token. Set this value > 1 for sampling decoding
+        }
        
+        if show_progress:
+            iterator = tqdm(self.documents_)
+        else:
+            iterator = self.documents_
      
-        for document in self.document_list:
+        for document in iterator:
+            if document in self.summaries_:
+                continue
+
             response = ollama.chat(
-                        model="llama2",
-                        options=generation_kwargs,
-                        messages=[
-                            {"role": "user", "content": f"Summarize this code:{document}"}
-                        ],
-                    )
-            
-            summarized_data[document] = response
-            self.corpus.append(summarized_data)
-        return self.corpus
+                model="llama2",
+                options=generation_kwargs,
+                messages=[
+                    {"role": "user", "content": f"Summarize this code:{document}"}
+                ],
+            )
+            self.summaries_[str(document)] = response['message']['content']
+
+            if save_file != None:
+                self.to_file(save_file)
+    
+    def to_file(self, filename):
+        with open(filename, 'wb+') as file:
+            pickle.dump(self, file)
+
+    @staticmethod
+    def from_file(filename):
+        with open(filename, 'rb') as file:
+            return pickle.load(file)
+    
+    def __getitem__(self, document):
+        if document in self.summaries_:
+            return self.summaries_[str(document)]
+        return None
